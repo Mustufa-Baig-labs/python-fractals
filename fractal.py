a858878c9f5cfaf764312ba9b139ec20abe1ae58
Mustafa Baig <mustafabaig.labs@gmail.com>
2024-11-06 00:40:30 +0530

    Add files via upload

--- fractal.py
+++ fractal.py
@@ -1,17 +1,8 @@
 import pygame
 pygame.init()
 
-try:
-    zoom=int(input("Zoom->"))
-    print("Program has begun...")
-except:
-    print("Switching to default...")
-    zoom=1
-
-
-width,height=1366,768
-        
-win=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+width,height=2100,1080
+win=pygame.display.set_mode((width,height))
 Clock=pygame.time.Clock()
 run=True
 
@@ -26,43 +17,40 @@ def f(z,c=[0,0]):
     return add(square(z),c)
     
     
-def is_fractal(z,iterations,blacks=0):
-    if iterations>3000:
-        iterations=3000
+def is_fractal(z,iterations=100,blacks=0):
+    if iterations>2500:
+        iterations=2500
         
     c=[0,0]
     for i in range(iterations):
         c=f(c,z)
         if c[0]>2 or c[1]>2:
-            result=(i/600)
+            return i/iterations
             
-            return result
-            
-        if i>(iterations/5):
-            if blacks>5:
+        if i>(iterations/4):
+            if blacks>7:
                 return -2
     return -1
     
     
 
+zoom=8
+
 
 while run:   
     win.fill((0,0,0))
-    pygame.draw.rect(win,((50,50,50)),((width-1,0),(1,height)))
-    calc=[(zoom*height),((1.75/zoom)-(0.15)),(zoom*50)]
-    print("Zoom =",zoom)
+    pygame.draw.rect(win,((50,50,50)),((width,0),(1,height)))
+    calc=[(zoom*1000),((1.75/zoom)-(0.15)),(zoom*15)]
     blacks=0
     for x in range(width):
-        if not(run):
-            break
         for y in range(height):
-            a=((x/calc[0])-(calc[1]))
-            b=((y/calc[0])-(0.64))
+            a=((x/(calc[0]))-(calc[1]))
+            b=((y/(calc[0]))-(0.65))
             
             if zoom>15:
                 r=is_fractal([a,b],calc[2],blacks)
             else:
-                r=is_fractal([a,b],750,blacks)
+                r=is_fractal([a,b],100,blacks)
              
             if r==0 or r==-1:
                 blacks+=1   
@@ -82,14 +70,7 @@ while run:
                     c=0
                 color=(c,c,c)
                 pygame.draw.rect(win,(color),((x,y),(1,1)))
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                run=False
-                
+        
         pygame.display.update()
         Clock.tick(60)
     zoom*=2
-
-    
-pygame.quit()
-
